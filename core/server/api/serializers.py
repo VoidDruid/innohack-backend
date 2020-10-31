888a68b5d782ed8d3f21835f21267b23124fda56
Igor Beschastnov <beschastnovigor@gmail.com>
2020-10-31 22:40:59 +0300

    u

--- core/server/api/serializers.py
+++ core/server/api/serializers.py
@@ -35,8 +35,6 @@ class SiteSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=250, allow_null=True)
     layout = serializers.JSONField(allow_null=True, validators=[validate_site_layout])
-    long = serializers.FloatField()
-    lat = serializers.FloatField()
     config = serializers.JSONField(allow_null=True, validators=[validate_site_config])
 
     def create(self, validated_data):
