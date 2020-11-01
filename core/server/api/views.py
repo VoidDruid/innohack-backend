7fa45543d86be334c0edf0ecaef8b65c5cb64b0b
Igor Beschastnov <beschastnovigor@gmail.com>
2020-11-01 10:32:19 +0300

    update

--- core/server/api/views.py
+++ core/server/api/views.py
@@ -120,35 +120,8 @@ class PositionView(APIView):
         return Response({'ok': True})
 
 
-from rest_framework.parsers import BaseParser
-from rest_framework import renderers
-from rest_framework.settings import api_settings
-from django.conf import settings
-from rest_framework.utils import json
-import codecs
-class MJSONParser(BaseParser):
-    media_type = 'application/json'
-    renderer_class = renderers.JSONRenderer
-    strict = api_settings.STRICT_JSON
-
-    def parse(self, stream, media_type=None, parser_context=None):
-        """
-        Parses the incoming bytestream as JSON and returns the resulting data.
-        """
-        parser_context = parser_context or {}
-        encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
-
-        try:
-            decoded_stream = codecs.getreader(encoding)(stream)
-            parse_constant = json.strict_constant if self.strict else None
-            return json.load(decoded_stream, parse_constant=parse_constant)
-        except ValueError as exc:
-            raise ParseError('JSON parse error - %s' % str(exc))
-
-
 class SensorReportView(APIView):
     queryset = SensorReport.objects.all()
-    parser_classes = [MJSONParser]
 
     def post(self, request):
         if isinstance(request.data, QueryDict):
